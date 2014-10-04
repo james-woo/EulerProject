bd21e7e355d68ca0c0a829ed2d9f893a8b0e838d
James Woo <james@Jamess-MacBook-Pro.local>
2014-10-04 13:24:28 -0700

    updated question

--- Problem1/Problem1.go
+++ Problem1/Problem1.go
@@ -1,6 +1,15 @@
-//written in go
-//James Woo
-//October 4 2014
+/*
+James Woo
+Problem 1
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 
+and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+
+Written in golang
+
+October 4 2014
+*/
 
 package main
 
